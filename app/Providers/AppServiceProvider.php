c61c9e4e921ce6ecdfd6a887e1ae985b1acf608d
Alden <aldendoank4@gmail.com>
2026-07-25 23:49:22 +0700

    Fix force HTTPS in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +23,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $request)
     {
         if (config('app.env') === 'production') {
+            $request->server->set('HTTPS', 'on');
             URL::forceScheme('https');
         }
     }
